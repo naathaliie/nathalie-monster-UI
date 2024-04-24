e6859e04ea899ba83c6cd1060aa3679479787b61
naathaliie <nathalielarsson@live.se>
2024-04-24 21:45:00 +0200

    allmonster

--- src/routes/AllMonstersPage.tsx
+++ src/routes/AllMonstersPage.tsx
@@ -1,5 +1,30 @@
+import { useContext } from "react";
+import { monsterContext } from "../components/context/MonsterContext";
+import "./AllMonstersPage.scss";
+
 const AllMonstersPage = () => {
-  return <div className="AllMonstersPage">Alla elever</div>;
+  const { state } = useContext(monsterContext);
+
+  //*Sortera monster efter förnamn i alfabetisk ordning
+  const sortedMonsters = state.monster.slice().sort((a, b) => {
+    return a.förnamn.localeCompare(b.efternamn);
+  });
+
+  return (
+    <div className="AllMonstersPage">
+      <h1>Alla monster på Monster UI</h1>
+
+      {sortedMonsters.map((m) => {
+        return (
+          <ul key={m.id}>
+            <li>
+              {m.förnamn} {m.efternamn}
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AllMonstersPage;
