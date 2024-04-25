1be1d4d0e75ad38048c9548f2dd694a71907631d
naathaliie <nathalielarsson@live.se>
2024-04-25 10:56:49 +0200

    try other things in context

--- src/components/context/MonsterContext.tsx
+++ src/components/context/MonsterContext.tsx
@@ -1,14 +1,11 @@
-import { createContext, useContext } from "react";
-import {
-  InitialMonsterType,
-  initialMonsterState,
-} from "../../types/monsterTypes";
+import { createContext } from "react";
+import { OneMonster, initialMonsterState } from "../../types/monsterTypes";
 import { Action } from "./monsterReducer";
-
+// Skapar en context för att hantera tillstånd
 export const monsterContext = createContext<{
-  state: InitialMonsterType;
+  state: OneMonster[]; // state är en array av OneMonster-objekt
   dispatch: React.Dispatch<Action>;
 }>({
-  state: initialMonsterState,
-  dispatch: () => null,
+  state: initialMonsterState.monster, // Inledande tillstånd
+  dispatch: () => null, // Standardfunktion som inte gör något
 });
--- src/components/context/monsterReducer.tsx
+++ src/components/context/monsterReducer.tsx
@@ -2,7 +2,7 @@ import { InitialMonsterType } from "../../types/monsterTypes";
 
 export type Action =
   | { type: typeof ACTIONS.ADD; payload: string }
-  | { type: typeof ACTIONS.REMOVE; payload: string };
+  | { type: typeof ACTIONS.REMOVE; payload: number };
 
 export const ACTIONS = {
   ADD: "add",
@@ -13,6 +13,14 @@ export const monsterReducer = (state: InitialMonsterType, action: Action) => {
   switch (action.type) {
     case ACTIONS.ADD:
       return state;
+
+    case ACTIONS.REMOVE:
+      console.log("Du kom till actions.remove");
+      return {
+        monster: state.monster.filter((m) => {
+          return m.id !== action.payload;
+        }),
+      };
 
     default:
       return state;
