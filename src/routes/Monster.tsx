497be978bb966fcf94c09da699a6b58b6c68534d
naathaliie <nathalielarsson@live.se>
2024-04-25 09:42:00 +0200

    monster-info

--- src/routes/Monster.tsx
+++ src/routes/Monster.tsx
@@ -27,10 +27,81 @@ const Monster = () => {
 
   return (
     <div className="Monster">
-      <h3>
-        {foundMonster.förnamn} {foundMonster.efternamn}
-      </h3>
-      <img src={`/src/assets/img/${foundMonster.img}`} alt={foundMonster.img} />
+      <div className="img-box">
+        <img
+          src={`/src/assets/img/${foundMonster.img}`}
+          alt={foundMonster.img}
+        />
+        <button>Radera monster</button>
+      </div>
+      <div className="info-box">
+        <h3>
+          {foundMonster.förnamn} {foundMonster.efternamn}
+        </h3>
+        <div className="skol-info">
+          <h4>Skol-info</h4>
+          <p>Program: {foundMonster.program}</p>
+          <p>Klass: {foundMonster.klass}</p>
+          <p>Kurser: {foundMonster.kurser.join(", ")}</p>
+          <p>
+            Betyg:{" "}
+            <ul key={foundMonster.id}>
+              {foundMonster.betyg.map((b) => {
+                return (
+                  <li>
+                    {b.kurs}: {b.betyg}
+                  </li>
+                );
+              })}
+            </ul>
+          </p>
+        </div>
+        <div className="monster-info">
+          <h4>Monster-info</h4>
+          <p>Hemstad: {foundMonster.hemstad}</p>
+          <p>Antal ögon: {foundMonster.utseende.antal_ögon}</p>
+          <p>Ögonfärg: {foundMonster.utseende.ögonfärg}</p>
+          <p>
+            {foundMonster.utseende.antal_tentakler > 0 &&
+              `Tentakler: ${foundMonster.utseende.antal_tentakler}`}
+          </p>
+          <p>
+            Hud: {foundMonster.utseende.hud}, {foundMonster.utseende.hudfärg}
+          </p>
+          <p>
+            {foundMonster.utseende.antal_horn > 0 &&
+              `Horn: ${foundMonster.utseende.antal_horn}`}
+          </p>
+          <p>Har päls: {foundMonster.utseende.päls ? "Ja" : "Ja"}</p>
+          <p>
+            {foundMonster.utseende.antal_horn > 0 &&
+              `Horn: ${foundMonster.utseende.antal_horn}`}
+          </p>
+          <p>
+            Magiska förmågor:{" "}
+            {foundMonster.utseende.magiska_förmågor.join(", ")}
+          </p>
+          <p>
+            Speciella och unika saker:{" "}
+            {foundMonster.utseende.speciella_unika_saker.join(", ")}
+          </p>
+          <br />
+          <h4>Kost:</h4>
+          <p>Äter kött: {foundMonster.kost.köttätare ? "Ja" : "Nej"}</p>
+          <p>Äter kött: {foundMonster.kost.växtätare ? "Ja" : "Nej"}</p>
+          <p>Äter kött: {foundMonster.kost.allätare ? "Ja" : "Nej"}</p>
+          <p>
+            Allergier:{" "}
+            {foundMonster.kost.allergier
+              ? foundMonster.kost.allergier.join(", ")
+              : "Nej"}
+          </p>
+          <br />
+          <h4>Övrigt:</h4>
+          <p>Intressen: {foundMonster.intressen.join(", ")}</p>
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 };
